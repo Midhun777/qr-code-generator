71c9db4726ff0d356858d0662aa3e0515c511127
midmat4828@gmail.com <midmat4828@gmail.com>
2024-01-29 23:27:31 +0530

    update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
 
 function generateQr() {
-	if (qrText.value.lenght > 0) {
+	if (qrText.value.length > 0) {
 		qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
 	} else {
 		qrText.classList.add('error');
