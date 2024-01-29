0fb3f1465a3b10699ecdf51b886da59f4449261b
midmat4828@gmail.com <midmat4828@gmail.com>
2024-01-29 23:14:35 +0530

    shake animation update

--- main.js
+++ main.js
@@ -3,5 +3,13 @@ let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
 
 function generateQr() {
-	qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+	if (qrText.value.lenght > 0) {
+		qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+	} else {
+		qrText.classList.add('error');
+		setTimeout(() => {
+			qrText.classList.remove('error');
+
+		}, 500);
+	}
 }
