d21ce4e34925caec95bbf1cf85b31fff12557044
midmat4828@gmail.com <midmat4828@gmail.com>
2024-01-30 14:43:55 +0530

    txt

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let imgBox = document.getElementById("imgBox");
 let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
+let link = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
 
 function generateQr() {
 	if (qrText.value.length > 0) {
